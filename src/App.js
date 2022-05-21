4d6e60efa3a20418fb420f0a47f19863cbd2133d
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-05-22 02:13:14 +0600

    components created and firebase setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <h2>Mama po</h2>
     </div>
   );
 }
---
+++ src/Pages/Auth/SingUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingUp = () => {
+    return (
+        <div>
+            <h2>Sign up</h2>
+        </div>
+    );
+};
+
+export default SingUp;
